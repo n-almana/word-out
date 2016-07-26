10b9de87ada25f7846d968a150c1357cacb5983d
Noora Al-Mana <nalmana.babson@gmail.com>
2016-07-26 18:22:32 -0400

    Search bar for posts working

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,15 @@
 class Post < ActiveRecord::Base
 	belongs_to(:user)
 
+	validates_presence_of(:text, :title) 
+
 	def title_and_author
 		"#{title} by #{user.email}"
 	end
+
+	def self.search(search)
+  		where("title LIKE ?", "%#{search}%") 
+  		where("text LIKE ?", "%#{search}%")
+	end
+
 end
